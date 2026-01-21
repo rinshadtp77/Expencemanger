8eab0e4d6c48b3e367b7dc114895956c9d3d3b82
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-01-21 15:19:50 +0530

    Functions Added

--- expences.py
+++ expences.py
@@ -1,5 +1,27 @@
 s=0
 expences=[]
+def create_expence():
+    payee=input("Enter Payee")
+    amount=input("Enter Amount")
+    purpose=input("Enter Purpose")
+    dt=input("Enter Date")
+    expences.append((payee,amount,purpose,dt))
+def show_expences():
+    #print(payee+" "+amount+" "+purpose+" "+dt)
+    #print(expences)
+    for item in expences:
+        print(item)
+def delete_expence():
+    id=int(input("Enter the index you want to delete"))
+    del expences[id]
+def update_expence():
+    id=int(input("Enter the index you want to update"))
+    payee=input("Enter Payee")
+    amount=input("Enter Amount")
+    purpose=input("Enter Purpose")
+    dt=input("Enter Date")
+    expences[id]=(payee,amount,purpose,dt)
+
 while(s==0):
     print("=====Menu=====")
     print("1=> For Insert")
@@ -8,25 +30,11 @@ while(s==0):
     print("4=> For Update")
     opt=int(input("Enter the option"))
     if(opt==1):
-        payee=input("Enter Payee")
-        amount=input("Enter Amount")
-        purpose=input("Enter Purpose")
-        dt=input("Enter Date")
-        expences.append((payee,amount,purpose,dt))
+        create_expence()
     if(opt==2):
-        #print(payee+" "+amount+" "+purpose+" "+dt)
-        #print(expences)
-        for item in expences:
-            print(item)
+        show_expences()
     if(opt==3):
-        id=int(input("Enter the index you want to delete"))
-        del expences[id]
+        delete_expence()
     if(opt==4):
-        id=int(input("Enter the index you want to update"))
-        payee=input("Enter Payee")
-        amount=input("Enter Amount")
-        purpose=input("Enter Purpose")
-        dt=input("Enter Date")
-        expences[id]=(payee,amount,purpose,dt)
-
+        update_expence()  
     s=int(input("Do you want to continue? press 0 For Yes"))
